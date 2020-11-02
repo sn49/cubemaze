1c5dcc62d85e434c94e902fb567f813ffd091dd6
sn49 <34567701+sn49@users.noreply.github.com>
2020-11-02 15:14:02 +0900

    git ignore 3차 적용

---
+++ cubemaze/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float maxspeed = 10f;
+
+    float haxis;
+    float vaxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        haxis = Input.GetAxis("Horizontal");
+        vaxis = Input.GetAxis("Vertical");
+
+        transform.Translate(haxis * maxspeed*Time.deltaTime, 0, vaxis * maxspeed * Time.deltaTime);
+    }
+}
