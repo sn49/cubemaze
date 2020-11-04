82192503b1088b8b5765039fe4f767bd0758d5e0
sn49 <34567701+sn49@users.noreply.github.com>
2020-11-04 09:56:22 +0900

    큐브 5가지 material

--- cubemaze/Assets/Scripts/GameManager.cs
+++ cubemaze/Assets/Scripts/GameManager.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int score;
-    public Stopwatch timer = new Stopwatch();
-    
-    
-
+ 
     // Start is called before the first frame update
     void Start()
     {
